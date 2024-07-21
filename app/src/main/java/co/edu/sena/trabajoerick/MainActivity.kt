41d071429aa3a1cd7e25dfa863aff978cceeba54
Andres suarez <suarezruizandresfelipe801@gmail.com>
2024-07-20 23:27:17 -0500

    se creo main

--- app/src/main/java/co/edu/sena/trabajoerick/MainActivity.kt
+++ app/src/main/java/co/edu/sena/trabajoerick/MainActivity.kt
@@ -4,44 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import co.edu.sena.trabajoerick.ui.theme.TrabajoerickTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TrabajoerickTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        }//setContent
+    }//onCreate
+}//MainActivity
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoerickTheme {
-        Greeting("Android")
-    }
-}
+//@preview
